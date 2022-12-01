db42a953d111595e58ed2c9cbacb058d650e50cc
nikoladev95 <nikola.milanovic.dev@gmail.com>
2022-12-01 20:44:33 +0100

    Mocha added

--- inbox/compile.js
+++ inbox/compile.js
@@ -1,1 +1,8 @@
-// compile code will go here
+const path = require('path');
+const fileSystem = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fileSystem.readFileSync(inboxPath, 'utf8');
+
+module.exports = solc.compile(source, 1).contracts[':Inbox'];
